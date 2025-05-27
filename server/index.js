7167fd2c8deb8bc8e3ebc464cd261229df3750fa
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-27 15:17:04 +0530

    Add clients array to store connected SSE clients

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+const clients = [];
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.listen(PORT, () => {
