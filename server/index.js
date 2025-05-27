068659a845354944b2aa67b735ddeb749861a5ec
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-27 16:53:57 +0530

    Generate random data every second and store in values

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.get('/stream', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
+ 
   res.flushHeaders();
 
   clients.push(res);
@@ -24,6 +24,14 @@ app.get('/stream', (req, res) => {
     if (idx !== -1) clients.splice(idx, 1);
   });
 });
+
+setInterval(() => {
+  const randomValue = Math.floor(Math.random() * 101);
+  const timestamp = new Date().toISOString();
+  const data = { value: randomValue, time: timestamp };
+
+  values.push(data);
+}, 1000);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
